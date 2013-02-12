2723dec635644c7bb66b2dddc78f10244ae5ae7a
bandrus <bandrus@CHS-ROBO-01.carmelunified.org>
2013-02-11 17:21:37 -0800

    Adding new commands needed by the CommandGroup ClimbLevelOne

---
+++ src/edu/wpi/first/wpilibj/templates/commands/ArmsRetractConditional.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST Team 2035, 2012. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.templates.ScraperBike;
+import edu.wpi.first.wpilibj.templates.subsystems.Arms;
+
+/**
+ *
+ * @author bradmiller
+ */
+public class ArmsRetractConditional extends CommandBase {
+    private Arms arm;
+    
+    public ArmsRetractConditional() {
+        arm = ScraperBike.getArms();
+        requires(arm);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        arm.move(Relay.Value.kReverse);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return !arm.isContacting();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
