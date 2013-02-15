ee7c4fc48641673005c3622443171b2a49bc8f91
TheTimeLord <brianandrus2@gmail.com>
2013-02-15 09:04:48 -0800

    Added code to ClimbPyramid.java

--- src/edu/wpi/first/wpilibj/templates/commands/ClimbPyramid.java
+++ src/edu/wpi/first/wpilibj/templates/commands/ClimbPyramid.java
@@ -12,18 +12,26 @@ package edu.wpi.first.wpilibj.templates.commands;
  * @author bradmiller
  */
 public class ClimbPyramid extends CommandBase {
-
+    private ClimbLevelOne levelOne;
+    private ClimbLevelTwo levelTwo;
+    private ClimbLevelThree levelThree;
+    
     public ClimbPyramid() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+     levelOne = new ClimbLevelOne();
+     levelTwo = new ClimbLevelTwo();
+     levelThree = new ClimbLevelThree();
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        levelOne.execute();
+        levelTwo.execute();
+        levelThree.execute();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,10 +41,12 @@ public class ClimbPyramid extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+    
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    
     }
 }
