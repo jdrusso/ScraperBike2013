5332a6eafc4b59314c9156b010fe164333e4868d
toastertaster <toastertaster@gmail.com>
2013-02-16 13:13:23 -0800

    Set a joystick button for manually extending and contracting the arms. Corrected the commands to trn motors off on arms.

--- src/edu/wpi/first/wpilibj/templates/commands/ArmsRetract.java
+++ src/edu/wpi/first/wpilibj/templates/commands/ArmsRetract.java
@@ -38,10 +38,12 @@ public class ArmsRetract extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+         arm.move(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+         arm.move(0);
     }
 }
