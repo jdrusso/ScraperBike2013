72165788d99dca41ab448abe828fb65ea6de570c
toastertaster <toastertaster@gmail.com>
2013-03-20 23:01:58 -0700

    Changed the file names to clarify the condition is on the stop or ending of the command.

---
+++ src/edu/wpi/first/wpilibj/templates/commands/ArmsExtendConditionalStopMove.java
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST Team 2035, 2013. All Rights Reserved.                  */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.templates.ScraperBike;
+import edu.wpi.first.wpilibj.templates.subsystems.Arms;
+import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+
+/** Used to extend the arms in the forward direction.
+ *
+ * @author Team 2035 Programmers
+ */
+public class ArmsExtendConditionalStopMove extends CommandBase {
+    private Arms arm;
+    private DriveTrain dt;
+    private int endcond;
+    
+    
+    /** Commands the Arms to extend.  
+     * Ends when the Arms extend to their forward limit.
+     *
+     * @param endCondition (not used)
+     */
+    public ArmsExtendConditionalStopMove(int endCondition) {
+        super("ArmsExtend");
+        arm = ScraperBike.getArms();
+        dt = ScraperBike.getDriveTrain();
+        requires(arm);
+        requires(dt);
+        endcond = endCondition;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if (arm.isLimitFore()) {
+            arm.move(0.0);
+        } else {
+            arm.move(1.0);
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+//        return false;
+        if (endcond == 1) {
+            return arm.isContacting();
+        } else if (endcond == 2) {
+            return arm.isLimitFore();
+        } 
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        arm.move(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+         arm.move(0.0);
+    }
+}
--- src/edu/wpi/first/wpilibj/templates/commands/ArmsRetractConditionalStopMove.java
+++ src/edu/wpi/first/wpilibj/templates/commands/ArmsRetractConditionalStopMove.java
@@ -10,25 +10,28 @@ import edu.wpi.first.wpilibj.templates.ScraperBike;
 import edu.wpi.first.wpilibj.templates.subsystems.Arms;
 import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
 
-/** we may not use this, it has errors currently
+/** Used for the climbing sequence to stop at a sensor.  Either the home sensor,
+ * or the aft position sensor.
  *
  * @author Team 2035 Programmers
  */
-public class ArmsRetractConditional extends CommandBase {
+public class ArmsRetractConditionalStopMove extends CommandBase {
     private Arms arm;
     private DriveTrain dt;
     private boolean started;
+    private int endcondition;
     
     /**
-     *
+     * @param endcond End condition (1 = home, 2 = aft)
      */
-    public ArmsRetractConditional() {
-        super("ArmsRetractConditional");
+    public ArmsRetractConditionalStopMove(int endcond) {
+        super("ArmsRetractConditionalStopMove");
         arm = ScraperBike.getArms();
         dt = ScraperBike.getDriveTrain();
         requires(arm);
         requires(dt);
         started = false;
+        endcondition = endcond;
     }
 
     // Called just before this Command runs the first time
@@ -37,9 +40,9 @@ public class ArmsRetractConditional extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run, allows arms to retract only if they are hooked on to the bar on the pyramid
     protected void execute() {
-        if (arm.isContacting()) {
+        //if (arm.isContacting()) {
             started = true;
-        }
+        //}
         if (started) {
             arm.move(-1);    
         }
@@ -48,17 +51,22 @@ public class ArmsRetractConditional extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return started;
+        if (endcondition == 1) {
+            // if either sensor is tripped, then stop.
+            return arm.isHomePosition() || arm.isLimitAft();
+        }
+        // otherwise only stop for the aft sensor.
+        return arm.isLimitAft();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-         arm.move(0);
+         arm.move(0.0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-         arm.move(0);
+         arm.move(0.0);
     }
 }
